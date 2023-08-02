80779a516108d76696893836ff3e42c937fa5aef
Carter Tomlenovich <cartertomlenovich@gmail.com>
2023-08-02 17:01:24 -0400

    base paper plugin

---
+++ src/main/java/dev/hyperlisk/diamondspleef/DiamondSpleef.java
@@ -0,0 +1,17 @@
+package dev.hyperlisk.diamondspleef;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DiamondSpleef extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
